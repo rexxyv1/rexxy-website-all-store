26c903d66a0e3d2cc95fa827b3d2bd83d4a19af0
rexxyv1 <rexxyv1@gmail.com>
2026-07-22 04:13:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
+const overlay = document.getElementById("overlay");
 
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+    overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+});
